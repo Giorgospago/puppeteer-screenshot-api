e36f22464b2a030f17cf503ca4cfbf9afee35bf8
Pagonoudis <giorgospago23@gmail.com>
2022-06-03 13:20:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+
+app.listen(3030);
+
+app.get('/', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: Number(req.query.width) || 1920,
+            height: Number(req.query.height) || 1080,
+            deviceScaleFactor: Number(req.query.scale) || 1
+        });
+
+        await page.goto(req.query.url);
+        const image = await page.screenshot();
+
+        return res.header("content-type", "image/jpeg").send(image);
+    } catch(err) {
+        return res.json({
+            success: false,
+            message: err.message
+        });
+    }
+});
