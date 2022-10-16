95e18e50c1596061599da7584778a531d87e551e
Pagonoudis <giorgospago23@gmail.com>
2022-10-16 14:22:31 +0300

    Add video recording

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM node:16-buster
+
+# We don't need the standalone Chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+# Install Google Chrome Stable and fonts
+# Note: this installs the necessary libs to make the browser work with Puppeteer.
+RUN apt-get update && apt-get install gnupg wget -y && \
+  wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+  apt-get update && \
+  apt-get install google-chrome-stable -y --no-install-recommends && \
+  rm -rf /var/lib/apt/lists/*
+#ADD https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64 /usr/local/bin/dumb-init
+#RUN chmod +x /usr/local/bin/dumb-init
+
+RUN apt update
+RUN apt install ffmpeg -y
+
+# Install node modules
+WORKDIR /var/www
+
+COPY . .
+
+VOLUME /var/www/public
+
+RUN npm i --silent --only=production
+
+CMD [ "node", "index.js" ]
+
+EXPOSE 3000
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const puppeteer = require('puppeteer');
+const {PuppeteerScreenRecorder} = require('puppeteer-screen-recorder');
+const uuid = require('uuid');
 const express = require('express');
+const path = require("path");
 const app = express();
+const {rm, calculateRatio} = require("./lib/helpers");
 
-app.listen(3030);
+const BROWSER_LAUNCH = {
+    headless: true,
+    executablePath: '/usr/bin/google-chrome',
+    args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+    ]
+};
 
-app.get('/', async (req, res) => {
+app.listen(3000);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Welcome to Puppeteer Screenshot and Video API"
+    });
+});
+
+app.get('/image', async (req, res) => {
     try {
-        const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/chromium-browser'
-        });
+        const browser = await puppeteer.launch(BROWSER_LAUNCH);
         const page = await browser.newPage();
 
         await page.setViewport({
@@ -17,13 +38,70 @@ app.get('/', async (req, res) => {
             deviceScaleFactor: Number(req.query.scale) || 1
         });
 
-        await page.goto(req.query.url, {"waitUntil" : "networkidle0"});
+        await page.goto(req.query.url, {waitUntil: "networkidle0"});
         const image = await page.screenshot();
 
-        browser.close();
-
+        await browser.close();
         return res.header("content-type", "image/jpeg").send(image);
-    } catch(err) {
+    } catch (err) {
+        console.error(err);
+        if (browser && browser.close) {
+            browser.close();
+        }
+        return res.json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+app.get('/video', async (req, res) => {
+    try {
+        const browser = await puppeteer.launch(BROWSER_LAUNCH);
+        const page = await browser.newPage();
+
+        const viewport = {
+            width: Number(req.query.width) || 1920,
+            height: Number(req.query.height) || 1080,
+            deviceScaleFactor: Number(req.query.scale) || 1
+        };
+        await page.setViewport(viewport);
+
+        await page.goto(req.query.url, {waitUntil: "networkidle0"});
+
+        const urlParts = new URL(req.query.url);
+        const videoConfig = {
+            followNewTab: true,
+            fps: 25,
+            ffmpeg_Path: '/usr/bin/ffmpeg',
+            videoFrame: {
+                width: viewport.width * viewport.deviceScaleFactor,
+                height: viewport.height * viewport.deviceScaleFactor,
+            },
+            videoCrf: 18,
+            videoCodec: 'libx264',
+            videoPreset: 'ultrafast',
+            videoBitrate: 1000,
+            autopad: {
+                color: 'black' | '#35A5FF',
+            },
+            aspectRatio: calculateRatio(viewport.width, viewport.height) || '4:3',
+        };
+        const recorder = new PuppeteerScreenRecorder(page, videoConfig);
+        const filename = `${urlParts.host}-${viewport.width}x${viewport.height}-scale.${viewport.deviceScaleFactor}-${uuid.v4()}.mp4`;
+        await recorder.start(path.join(__dirname, "public/videos", filename));
+
+        await page.waitForTimeout(3000);
+
+        await recorder.stop();
+        await browser.close();
+
+        return res.json({
+            success: true,
+            url: req.protocol + '://' + req.get('host') + "/videos/" + filename
+        });
+    } catch (err) {
+        console.error("ERROR: ", err);
         if (browser && browser.close) {
             browser.close();
         }
